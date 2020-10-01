3c07e4c4e5d93a12ba2920ae265af3bcada41eff
Akhil-star <akhilakki1305@gmail.com>
2020-10-01 20:42:24 +0530

    Choose Letter(X/0)

--- src/main/java/com/cg/tictactoegame/TicTacToeGame.java
+++ src/main/java/com/cg/tictactoegame/TicTacToeGame.java
@@ -1,17 +1,26 @@
 package com.cg.tictactoegame;
 
-public class TicTacToeGame 
-{
-	static char[] board = new char[10];
-	private static char[] createBoard() {
-		for (int i = 1; i < board.length; i++) {
-			board[i] = ' ';
+import java.util.Scanner;
+
+public class TicTacToeGame {
+	static Scanner sc = new Scanner(System.in);
+
+	// UC2chooseLetter
+	private static char chooseLetter() {
+		System.out.println("Player's Letter (X/O) :");
+		char letter = Character.toUpperCase(sc.next().charAt(0));
+		if (letter == 'X' || letter == 'O') {
+			return letter;
+		} else {
+			System.out.println("Invalid Character");
+			return chooseLetter();
 		}
-		return board;
 	}
-    public static void main( String[] args )
-    {
-     createBoard();
-     System.out.println("Board is created");
-    }
+
+	public static void main(String[] args) {
+		char playerLetter = chooseLetter();
+		char computerLetter = (playerLetter == 'X') ? 'O' : 'X';
+		System.out.println("Player's Letter : " + playerLetter + "\nComputer's Letter : " + computerLetter);
+
+	}
 }
