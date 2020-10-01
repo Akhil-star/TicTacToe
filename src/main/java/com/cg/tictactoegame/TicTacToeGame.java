0444934553efe1f009074564cfa534afee93912c
Akhil-star <akhilakki1305@gmail.com>
2020-10-01 20:52:09 +0530

    Show/Display Board

--- src/main/java/com/cg/tictactoegame/TicTacToeGame.java
+++ src/main/java/com/cg/tictactoegame/TicTacToeGame.java
@@ -3,7 +3,16 @@ package com.cg.tictactoegame;
 import java.util.Scanner;
 
 public class TicTacToeGame {
+	static char[] board = new char[10];
 	static Scanner sc = new Scanner(System.in);
+
+	// UC1createBoard
+	private static char[] createBoard() {
+		for (int i = 1; i < board.length; i++) {
+			board[i] = ' ';
+		}
+		return board;
+	}
 
 	// UC2chooseLetter
 	private static char chooseLetter() {
@@ -17,10 +26,20 @@ public class TicTacToeGame {
 		}
 	}
 
+	// UC3showBoard
+	private static void showBoard(char[] board) {
+		System.out.println("| " + board[1] + " | " + board[2] + " | " + board[3] + " |");
+		System.out.println("|-----------|");
+		System.out.println("| " + board[4] + " | " + board[5] + " | " + board[6] + " |");
+		System.out.println("|-----------|");
+		System.out.println("| " + board[7] + " | " + board[8] + " | " + board[9] + " |");
+	}
+
 	public static void main(String[] args) {
 		char playerLetter = chooseLetter();
 		char computerLetter = (playerLetter == 'X') ? 'O' : 'X';
 		System.out.println("Player's Letter : " + playerLetter + "\nComputer's Letter : " + computerLetter);
-
+		createBoard();
+		showBoard(board);
 	}
 }
