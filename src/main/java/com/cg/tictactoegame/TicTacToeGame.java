94c8fd152fdbbb19fcdc38f87e2335eef8a54926
Akhil-star <akhilakki1305@gmail.com>
2020-10-01 20:25:23 +0530

    Welcome msg

---
+++ src/main/java/com/cg/tictactoegame/TicTacToeGame.java
@@ -0,0 +1,9 @@
+package com.cg.tictactoegame;
+
+public class TicTacToeGame 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to tictactoe game" );
+    }
+}
